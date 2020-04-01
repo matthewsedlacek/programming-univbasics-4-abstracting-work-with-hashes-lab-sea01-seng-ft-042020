704b92ffcedf39870350cb59a49306909c68e704
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-04-01 22:31:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,8 @@
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-hash{[key]}
+hash[key]
 end
+nil
 
 
 def read_from_hash(hash, key)
